98c3a9613327d958b2c87dca4798c624eb49355a
阿鱼与猫 <ayuyumao@163.com>
2022-11-23 19:03:34 +0800

    提交ch02章
    源代码

--- ch02/exe02_08.cpp
+++ ch02/exe02_08.cpp
@@ -8,8 +8,8 @@ int main()
    	double payment = 0;
     
     cout << "欠费金额 " << amount << endl;
-    cout << "	户名 长春慧鹏石油化工科技有限公\n\t司" << endl;
-    cout << "	地址 吉林省长春市二道区中意之尊\n\t小区4#楼商铺101" << endl;
+    cout << "    户名 长春慧鹏石油化工科技有限公\n\t司" << endl;
+    cout << "    地址 吉林省长春市二道区中意之尊\n\t小区4#楼商铺101" << endl;
     cout << "截至时间 19日04时" << endl;
 
     cout << "请输入缴入金额(大于574): ";
